e1d88f37a851f11905ecc02bbd3984860e37568f
SilverEzredes <68148231+SilverEzredes@users.noreply.github.com>
2024-12-18 01:55:07 +0100

    v1.00.1
    Fixed an issue where tabs would be used instead of whitespaces.
    Fixed an issue where multiple anims would be set for one entity at once.

--- SILVER_XL_Gen.py
+++ SILVER_XL_Gen.py
@@ -1,18 +1,32 @@
 import os
 
 # Set output file path and name
-outputFilePath = "X:\\Cyberpunk 2077\\archive\\pc\\mod\\"
+outputFilePath = "G:\\Cyberpunk 2077\\archive\\pc\\mod\\"
 outputFileName = "YourCoolXLFile"
 
 # Define ent file list
 entList = [ 
-    "path\\to\\some.ent",
-    "path\\to\\some_other.ent",
+    #FemV and FemNPC ents:
+    "base\\characters\\entities\\player\\photo_mode\\player_wa_photomode.ent",
+    "ep1\\characters\\entities\\player\\photo_mode\\player_wa_photomode_ep1.ent",
+    "base\\characters\\entities\\player\\photo_mode\\alt_cunningham\\alt_photomode.ent",
+    "base\\characters\\entities\\player\\photo_mode\\blue_moon\\bmuc_photomode.ent",
+    "base\\characters\\entities\\player\\photo_mode\\evelyn_parker\\evelyn_photomode.ent",
+    "base\\characters\\entities\\player\\photo_mode\\hanako_arasaka\\hanako_photomode.ent",
+    "base\\characters\\entities\\player\\photo_mode\\judy_alvarez\\judy_photomode.ent",
+    "base\\characters\\entities\\player\\photo_mode\\lizzy_wizzy\\lizzy_photomode.ent",
+    "base\\characters\\entities\\player\\photo_mode\\meredith_stout\\meredith_photomode.ent",
+    "ep1\\characters\\entities\\player\\photo_mode\\myers\\myers_photomode.ent",
+    "base\\characters\\entities\\player\\photo_mode\\rogue_amendiares\\old_rogue_photomode.ent",
+    "base\\characters\\entities\\player\\photo_mode\\panam_palmer\\panam_photomode.ent",
+    "base\\characters\\entities\\player\\photo_mode\\purple_force\\pfuc_photomode.ent",
+    "base\\characters\\entities\\player\\photo_mode\\red_menace\\rmuc_photomode.ent",
+    "base\\characters\\entities\\player\\photo_mode\\rogue_amendiares\\young_rogue_photomode.ent",
+    "ep1\\characters\\entities\\player\\photo_mode\\songbird\\songbird_photomode.ent",
 ]
 # Define anims to append
 animList = [ 
-    "path\\to\\some.anims",
-    "path\\to\\some_other.anims",
+    "base\\animations\\ui\\photomode\\pmu\\photomode__female__idle_zwei_alt.anims",
 ]
 
 print(f"\nSILVER-XL-GEN |==============================================================|")
@@ -23,9 +37,9 @@ if not os.path.exists(outputFilePath):
 outputLines = []
 outputLines.append("animations:")
 for ent in entList:
-    outputLines.append(f"\t- entity: {ent}")
     for anim in animList:
-        outputLines.append(f"\t\tset: {anim}")
+        outputLines.append(f" - entity: {ent}")
+        outputLines.append(f"   set: {anim}")
 
 outputFile = os.path.join(outputFilePath, outputFileName + ".archive.xl")
 with open(outputFile, "w") as file:
